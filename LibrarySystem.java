1dd6342496e8075c3ac5ce64682529f4809e3420
Haani <148327353+haani1224@users.noreply.github.com>
2025-06-21 23:41:35 +0800

    Update LibrarySystem.java

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -15,6 +15,18 @@ class Book {
         this.isAvailable = true;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
     public void setAvailable(boolean status) {
         this.isAvailable = status;
     }
@@ -49,6 +61,19 @@ class Library {
     public void addBook(String title, String author, int isbn) {
         Book book = new Book(title, author, isbn);
         books.add(book);
+    }
+    
+    public void listAvailableBook() {
+        for(int i = 0; i < books.size(); i++) {
+            if (books.get(i).isAvailable()) {
+                System.out.println((i + 1) + ". ");
+                System.out.println(books.get(i).getTitle());
+                System.out.println(books.get(i).getAuthor());
+                System.out.println(books.get(i).getIsbn());
+                System.out.println();   
+            }
+        }
+        
     }
 
     public void displayInfo() {
